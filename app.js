feff8290dc170e105dccc9cd4efd713a9342e2d6
Zen-Coderon <gaganreddyedu@gmail.com>
2025-05-09 18:25:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const p1s = document.querySelector('#p1s');
+const p2s = document.querySelector('#p2s');
+const p1b = document.querySelector('#p1b');
+const p2b = document.querySelector('#p2b');
+const re = document.querySelector('#reset');
+const slist = document.querySelector("#slist");
+let p1scr = 0;
+let p2scr = 0;
+let isGameOver = false;
+let winScr = 3;
+p1b.addEventListener('click',() => {
+    if(! isGameOver){
+        p1scr++;
+        if(p1scr === winScr){
+            isGameOver = true;
+            p1s.classList.add('winner');
+            p2s.classList.add('looser');
+        }
+        p1s.innerText = p1scr;
+    }
+})
+slist.addEventListener('change',function() {
+   winScr = parseInt(this.value);
+    reset();
+})
+p2b.addEventListener('click',() => {
+    if(! isGameOver){
+        p2scr++;
+        if(p2scr === winScr){
+            isGameOver = true;
+            p2s.classList.add('winner');
+            p1s.classList.add('looser');
+        }
+        p2s.innerText = p2scr;
+    }
+})
+re.addEventListener('click', reset);
+
+function reset (){
+    isGameOver = false;
+    p1scr=0;
+    p2scr=0;
+    p1s.innerText = p1scr;
+    p2s.innerText = p2scr;
+    p1s.classList.remove('winner', 'looser');
+    p2s.classList.remove('winner', 'looser');
+
+}
